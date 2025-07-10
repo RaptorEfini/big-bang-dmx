125d5d25e9bb7c8f11aa0f1b363215489c5cda52
JhonFM <raptorf2286@gmail.com>
2025-07-09 21:28:19 -0500

    feat(restart): add first apd40 grid

---
+++ init-darkglow.js
@@ -0,0 +1,8 @@
+// Import the components from the darkglow.js file
+import { registerComponents } from './ui/darkglow/darkglow.js';
+
+// Register all components with the default 'darkglow' prefix
+registerComponents();
+
+// Alternatively, you can use a custom prefix
+// registerComponents('custom');
